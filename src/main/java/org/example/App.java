079d5bd9e9ac0fe463588fb294f63e73c34f7913
Esteban Leyte-Vidal <EstebanLV@Knights.ucf.edu>
2021-09-12 14:53:10 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,60 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Esteban Leyte-Vidal
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner (System.in);
+
+        int monthChoice;
+
+        System.out.println("Enter the number of the month" );
+        monthChoice = scanner.nextInt();
+
+        switch(monthChoice)
+        {
+            case 1 :
+                System.out.println("The name of the month is January");
+                break;
+            case 2 :
+                System.out.println("The name of the month is February");
+                break;
+            case 3 :
+                System.out.println("The name of the month is March");
+                break;
+            case 4 :
+                System.out.println("The name of the month is April");
+            case 5:
+                System.out.println("The name of the month is May");
+                break;
+            case 6:
+                System.out.println("The name of the month is June");
+                break;
+            case 7:
+                System.out.println("The name of the month is July");
+                break;
+            case 8:
+                System.out.println("The name of the month is August");
+                break;
+            case 9:
+                System.out.println("The name of the month is September");
+                break;
+            case 10:
+                System.out.println("The name of the month is October");
+                break;
+            case 11:
+                System.out.println("The name of the month is November");
+                break;
+            case 12:
+                System.out.println("The name of the month is December");
+                break;
+
+        }
+    }
+}
